9e753f93c7d4d9360e85595b653b96cfb10e86c8
Daniel Bennington <dbennington125@gmail.com>
2020-05-11 14:34:09 -0700

    table update

--- data/exercises.js
+++ data/exercises.js
@@ -3,26 +3,47 @@ module.exports = [
     name: 'bench press',
     weight: 250,
     is_fullbody: false,
-    type: 'resistance'
+    type_id: 1
   },
   {
     name: 'squats',
     weight: 180,
     is_fullbody: true,
-    type: 'resistance'
+    type_id: 1
   },
   {
     name: 'deadlift',
     weight: 350,
     is_fullbody: true,
-    type: 'resistance'
+    type_id: 1
     
   },
   {
-    name: 'jump jacks',
+    name: 'jumping jacks',
     weight: 0,
     is_fullbody: false,
-    type: 'calisthenics'
+    type_id: 2
     
+  }, 
+  {
+    name: 'running',
+    weight: 0,
+    is_fullbody: false,
+    type_id: 3
+
+  },
+  {
+    name: 'plyometric jumps',
+    weight: 0,
+    is_fullbody: false,
+    type_id: 2
+    
+  },
+  {
+    name: 'wind sprints',
+    weight: 0,
+    is_fullbody: false,
+    type_id: 3
+
   }
 ];
